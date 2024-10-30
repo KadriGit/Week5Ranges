ae25b1380d51b1430908150917e7334ad485920b
KadriGit <kadri.viira@tktk.ee>
2024-10-30 18:24:18 +0200

    final code

--- Week5Ranges/Program.cs
+++ Week5Ranges/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0 (or below) - freezin
+// from 0 to 5 - cold
+// from 5 to 10 - chilly
+// from 10 to 15  - cool
+// from 15 to 28 - warm
+// above 28 - boilong hot
+
+Console.WriteLine("Enter the current temp: ");
+int currentTemp = Int32.Parse(Console.ReadLine());
+
+if(currentTemp <= 0){
+    Console.WriteLine("Freezing");
+}
+else if(currentTemp > 0 && currentTemp < 5){
+    Console.WriteLine("Cold");
+}
+else if (currentTemp >= 5 && currentTemp < 10)
+{
+    Console.WriteLine("Chilly");
+}
+else if (currentTemp >= 10 && currentTemp < 15)
+{
+    Console.WriteLine("Cool");
+}
+else if (currentTemp >= 15 && currentTemp < 28)
+{
+    Console.WriteLine("Warm");
+}
+else
+{
+    Console.WriteLine("Boling hot");
+}
